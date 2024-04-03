1cf791b96b4802550ff37030726d5d0d5a57cd3f
JSun <65435933+saldandpaper@users.noreply.github.com>
2024-04-02 20:11:50 -0600

    feat almost done

--- script/globalScript.js
+++ script/globalScript.js
@@ -1,20 +1,71 @@
+let todayInit = new Date();
+let today = {
+    year: todayInit.getFullYear(),
+    month: todayInit.getMonth(),
+    day: todayInit.getDate()
+}
+
+let latitude, longitude, date;
+let displayLat = document.getElementById("display-lat");
+let displayLon = document.getElementById("display-lon");
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setCoords);
+}
+else {
+    displayLat.innerHTML = "Coordinates not available."
+    displayLon.innerHTML = "Coordinates not available."
+}
+
+function setCoords(position){
+    displayLat.innerHTML = position.coords.latitude;
+    displayLon.innerHTML = position.coords.longitude;
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    printCoords(); // Call function to print latitude and longitude
+} 
+
+if (!today.month[1]){
+    today.month = "0" + today.month;
+}
+if (!today.day[1]){
+    today.day = "0" + today.day;
+}
+date = today.year + "-" + today.month + "-" + today.day;
+
+console.log("date: " + date);
+
+function printCoords() {
+    console.log("lat: " + latitude, ", lon: " + longitude);
+}
+
 document.getElementById('coords-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
 
     // Get latitude and longitude values from the form
-    const latitude = document.getElementById('lat').value;
-    const longitude = document.getElementById('lon').value;
+    let inputLatitude = document.getElementById("lat").value;
+    let inputLongitude = document.getElementById("lon").value;
 
+    if (!inputLatitude || !inputLongitude) {
+        inputLatitude = latitude;
+        inputLongitude = longitude;
+    }
+
+    let inputDate = document.getElementById("date").value;
+
+    if (inputDate) {
+        date = inputDate;
+    }
 
     // stan grad: 51.06460095300076, -114.08923968526184
 
-
-    // Replace 'YOUR_API_KEY' with your actual NASA API key
     const apiKey = '0nsB9sfNIgdaM94keBc6RI1qZdXlg3sQcYpWwBvW';
 
-    // Construct the API request URL
-    // const apiUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&date=2024-03-26&api_key=${apiKey}`;
-    const apiUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&date=2014-02-01&api_key=${apiKey}`
+    const apiUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&dim=1&date=${date}&api_key=${apiKey}`
+    
+    // const apiUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&date=2014-02-01&dim=0.5&api_key=${apiKey}`
+    
+    // const apiUrl = `https://api.nasa.gov/planetary/earth/imagery?lon=${longitude}&lat=${latitude}&dim=1&api_key=${apiKey}`
     console.log(apiUrl);
 
 
